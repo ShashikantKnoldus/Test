14f49cb252840e933e9b5be4db3ba8b352b1c797
ShashikantKnoldus <shashikant.tyagi@knoldus.com>
2021-07-19 09:02:15 +0530

    print name

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 class HelloWorld{
     public static void main(string args[]){
         System.out.println("hello world");
-        System.out.println();
+        System.out.println("");
+        System.out.println("shashikant");
     }
 }
